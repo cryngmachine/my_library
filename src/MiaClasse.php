d1be861d5ca77a4f8d2ab018ed2ca0e4d1ac6db9
Vittorio Persico <vpersicosw@gmail.com>
2025-03-27 19:02:36 +0100

    Inizializza libreria

---
+++ src/MiaClasse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MiaLibreria;
+
+class MiaClasse {
+    public function saluta($nome) {
+        return "Ciao, " . $nome . "! ciao";
+    } 
+}
+
+
+
+/*echo "# my_library" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cryngmachine/my_library.git
+git push -u origin main
+
+…or push an existing repository from the command line
+git remote add origin https://github.com/cryngmachine/my_library.git
+git branch -M main
+git push -u origin main*/
